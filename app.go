701893af9bdf4a58368dd62610c68a4db339c531
XS <devbycm@gmail.com>
2019-04-18 12:42:29 +1000

    update

--- app.go
+++ app.go
@@ -1,6 +1,58 @@
 package main
 
-func main() {
-	new(App).Action(":port", func(c *Context) {
-	}).Run()
+import (
+	"fmt"
+	"strings"
+)
+
+func check(err error, prompts ...string) bool {
+	if err != nil {
+		fmt.Println(strings.Join(prompts, ": ")+":", err)
+		return true
+	}
+	return false
 }
+
+func main() {
+	new(App).
+		Action("proxy :public :tunnel", proxy).
+		Action("server :tunnel", server).
+		Run()
+}
+
+func proxy(c *Context) {
+	p := c.Get("public")
+	t := c.Get("tunnel")
+	fmt.Println("Listen:", p)
+}
+
+func server(c *Context) {
+}
+
+//func proxy(c *Context) {
+//	fmt.Println("==", "waiting for registration...")
+//	var u *url.URL
+//	var rp *httputil.ReverseProxy
+//	var l sync.Mutex
+//
+//	fmt.Println(2)
+//	r := gin.New()
+//	r.GET("/rp", func(c *gin.Context) {
+//		addr := c.Query("addr")
+//		u, err := url.Parse(addr)
+//		check(err, "addr")
+//
+//		l.Lock()
+//		fmt.Println("=>", addr)
+//		rp = httputil.NewSingleHostReverseProxy(u)
+//		c.String(http.StatusOK, "=>"+addr)
+//		l.Unlock()
+//	})
+//	r.NoRoute(func(c *gin.Context) {
+//		fmt.Println(u)
+//		if rp != nil {
+//			rp.ServeHTTP(c.Writer, c.Request)
+//		}
+//	})
+//	check(r.Run(c.Get("addr")))
+//}
--- cli.go
+++ cli.go
@@ -8,8 +8,9 @@ import (
 )
 
 type App struct {
-	patterns []*actionPattern
-	context  *Context
+	patterns  []*actionPattern
+	context   *Context
+	execCount int
 }
 
 type Context struct {
@@ -22,7 +23,7 @@ func (c *Context) Get(key string) string {
 	return c.KV[key]
 }
 
-func (c *Context) Int(key string) int {
+func (c *Context) GetInt(key string) int {
 	v, err := strconv.Atoi(c.KV[key])
 	if err != nil {
 		panic("value type illegal (expect int): " + key + " " + c.KV[key])
@@ -72,11 +73,14 @@ func (app *App) Action(pattern string, h Handler) *App {
 func (app *App) Run() *App {
 	for _, p := range app.patterns {
 		if kv, ok := p.resolve(os.Args[1:]); ok {
-			fmt.Println(ok)
 			app.context = &Context{Path: os.Args[0], Args: os.Args[1:], KV: kv}
 			p.handler(app.context)
+			app.execCount++
 			break
 		}
+	}
+	if app.execCount == 0 {
+		fmt.Println("no action executed.")
 	}
 	return app
 }
